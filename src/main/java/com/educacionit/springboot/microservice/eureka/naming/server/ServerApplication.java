5d41b40549cf8395c02a5a83cfc1a0eff011a290
Raul Pena <raul.pena@gmail.com>
2018-12-17 15:16:47 -0300

    Initial import...

---
+++ src/main/java/com/educacionit/springboot/microservice/eureka/naming/server/ServerApplication.java
@@ -0,0 +1,18 @@
+
+package com.educacionit.springboot.microservice.eureka.naming.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerApplication {
+
+	public static void main(String[] args) {
+
+	    SpringApplication.run (ServerApplication.class, args);
+	}
+}
